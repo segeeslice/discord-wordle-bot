796dca2f157de29bcc234b503fba23e6f358d4fd
Dustin Seger <segeeslice@pop-os.localdomain>
2023-01-12 13:15:30 -0500

    Add Wordle result message parsing

--- src/listeners/messageCreate.ts
+++ src/listeners/messageCreate.ts
@@ -1,16 +1,24 @@
 import { Listener } from '@sapphire/framework';
 import type { Message } from 'discord.js';
 
+const WORDLE_REGEX: RegExp = /(Wordle) (\d+) (\d\/\d)/;
+
 // Listener for the messageCreate event from discord.js:
 // https://discord.js.org/#/docs/discord.js/v13/class/Client?scrollTo=e-messageCreate
 export class MessageCreateListener extends Listener {
+
     public constructor(context: Listener.Context, options: Listener.Options) {
         super(context, {
             ...options,
         });
     }
 
-    public run(message: Message) {
-        console.log("Received message: " + message);
+    public async run(message: Message) {
+        if (message.author.bot) return;
+        if (!message.content) return;
+
+        if (message.content.match(WORDLE_REGEX)) {
+            message.react('👌');
+        }
     }
 }
