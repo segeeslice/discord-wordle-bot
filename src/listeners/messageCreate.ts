876c024e1264907687220925bea6150ae8e61399
Dustin Seger <segeeslice@pop-os.localdomain>
2023-01-13 13:00:09 -0500

    Remove old arc addition listener

--- src/listeners/messageCreate.ts
+++ src/listeners/messageCreate.ts
@@ -1,11 +1,9 @@
 import { Listener } from '@sapphire/framework';
 import type { Message } from 'discord.js';
 
-import wordleArcParser from '../wordleArcParser';
 import wordleResultParser from '../wordleResultParser';
 import storage from '../storage';
 import WordleResult from '../classes/WordleResult';
-import WordleArc from '../classes/WordleArc';
 
 // Listener for the messageCreate event from discord.js:
 // https://discord.js.org/#/docs/discord.js/v13/class/Client?scrollTo=e-messageCreate
@@ -22,7 +20,6 @@ export class MessageCreateListener extends Listener {
         if (!message.content) return;
 
         this.listenForWordleResult(message);
-        this.listenForWordleArc(message);
     }
 
     private async listenForWordleResult(message: Message) {
@@ -33,14 +30,6 @@ export class MessageCreateListener extends Listener {
         if (result == undefined) return;
 
         storage.saveWordleResult(result);
-        message.react('👌');
-    }
-
-    private async listenForWordleArc(message: Message) {
-        let arcResult: WordleArc | undefined = wordleArcParser.parseArcInformation(message.content);
-
-        if (arcResult == undefined) return;
-        storage.saveArcInformation(arcResult);
         message.react('👌');
     }
 }
